955c59a9a0cea0a357e7d39dd1073547fe37210e
Enrico <enrico.voltan@mail.polimi.it>
2020-11-19 18:46:59 +0100

    CBFFWF

--- datasets/load_tables.py
+++ datasets/load_tables.py
@@ -15,8 +15,24 @@ def rowSplit(rowString):
     return tuple(indexes), value
 
 
-def load_ICM_csr(tuples=False):
+def load_ICM_csr():
     ICM_file = open_file("data_ICM_title_abstract.csv")
+    ICM_file.seek(0)
+    ICM_indexes = []
+    ICM_values = []
+    ICM_file.readline()
+    for line in ICM_file:
+        i, v = rowSplit(line)
+        ICM_indexes.append(i)
+        ICM_values.append(v)
+    data = np.array(ICM_indexes)
+    result = np.zeros((data[:, 0].max() + 1, data[:, 1].max() + 1), dtype=float)
+    result[data[:, 0], data[:, 1]] = ICM_values
+    ICM_all = sps.coo_matrix(result)
+    return ICM_all.tocsr()
+
+def load_clustered_ICM_csr():
+    ICM_file = open_file("clustered_features_ICM.csv")
     ICM_file.seek(0)
     ICM_indexes = []
     ICM_values = []
