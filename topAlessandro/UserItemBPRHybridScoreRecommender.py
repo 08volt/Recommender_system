da5d5f4939c909172c631a704fead4d7fdf34ce9
Contedigital <alessandro3.contini@mail.polimi.it>
2020-12-02 17:28:31 +0100

    Agg con new hybrid e best method since now

--- topAlessandro/UserItemBPRHybridScoreRecommender.py
+++ topAlessandro/UserItemBPRHybridScoreRecommender.py
@@ -1,29 +1,31 @@
 
-#import stuff
 from KNN.ItemKNNCFRecommender import ItemKNNCFRecommender
 from SLIM_BPR.Cython.SLIM_BPR_Cython import SLIM_BPR_Cython
 from KNN.UserKNNCFRecommender import UserKNNCFRecommender
 from Base.NonPersonalizedRecommender import TopPop
 from Base.BaseRecommender import BaseRecommender
+from GraphBased.P3alphaRecommender import P3alphaRecommender
 from operator import add
 import numpy as np
 from Base.DataIO import DataIO
 
-#for manually change parameters -- NOT IMPLEMENTED YET
 HybridRecommender_params = {
-    'a': 1.0,  # userCF
-    'b': 1.0,  # SLIM
-    'c': 1.0,  # itemCF
+    'a': 1.0,  # p3alpha
+    'b': 1.0,  # userCF
+    'c': 1.0,  # SLIM BPR
+    'd': 1.0   # itemCF
 }   
 
-def sumScores(s1,s2,s3):
+def sumScores(s1,s2,s3,s4):
     s1= list(map(add, s1, s2) )
     s1= list(map(add, s1, s3) )
+    s1= list(map(add, s1, s4) )
     return s1
 
-def sumScoresWeights(s1,s2,s3,ws1,ws2,ws3):
+def sumScoresWeights(s1,s2,s3,s4,ws1,ws2,ws3,ws4):
     s1= list(map(add, ws1*s1, ws2*s2) )
     s1= list(map(add, s1, ws3*s3) )
+    s1= list(map(add, s1, ws4*s4) )
     return s1
 
 import sys, os
@@ -39,7 +41,7 @@ def enablePrint():
 class UserItemBPRHybridScoreRecommender(BaseRecommender):
     
     RECOMMENDER_NAME = "UserItemBPRHybridScoreRecommender"
-    #initialization 
+    #inzializzo tutti i modelli che uso
     def __init__(self, URM_train):
 
         super(UserItemBPRHybridScoreRecommender, self).__init__(URM_train)
@@ -53,11 +55,10 @@ class UserItemBPRHybridScoreRecommender(BaseRecommender):
         self.itemKNNCF = ItemKNNCFRecommender(self.URM_train)
         self.userRecommender = UserKNNCFRecommender(self.URM_train)
         self.topPop = TopPop(self.URM_train)
-        
+        self.p3alpha=P3alphaRecommender(URM_train)
 
     #qui a,b,c sono i pesi del mix degli scores
-    #fit function
-    def fit(self, URM_train=None,ws1=1,ws2=1,ws3=1):
+    def fit(self, URM_train=None,ws1=1,ws2=1,ws3=1,ws4=1):
         print("--------FITTING IN PROGRESS...-------")
         #blockPrint()
 
@@ -67,16 +68,20 @@ class UserItemBPRHybridScoreRecommender(BaseRecommender):
         self.ws1 = ws1
         self.ws2 = ws2
         self.ws3 = ws3
+        self.ws4 = ws4
 
         self.recommenderBPR.fit(epochs=300, batch_size=1, sgd_mode='sgd', learning_rate=0.001, positive_threshold_BPR=0.8)
-        self.itemKNNCF.fit(shrink=110, topK=110)
-        self.userRecommender.fit(shrink=0.5, topK=150)
+        self.itemKNNCF.fit(shrink=51, topK=370) #MAP=0.041
+        self.userRecommender.fit(shrink=0.5, topK=150)#MAP=0.056 #sbagliato, va sistemato
         self.topPop.fit()
+        self.p3alpha.fit(alpha=0.547615508822564,topK=500)#'MAP': 0.058 circa
+
+
         #enablePrint()
         print("------FITTING END, SIAMO GROSSISSIMI ------")
         
     #qui calcolo score per ogni metodo e sommo e tutte quelle belle cose
-    #questa funzione è chiamat dentro reccommend e ritorna lo score degli items
+    #penso basti fare gli scores e basta , poi se chiama reccommend ci pensa lui a dire quali hanno buoni scores 
     def _compute_item_score(self, user_id_array, items_to_compute=None):
         item_weights = np.zeros((len(user_id_array), self.URM_train.shape[1]), dtype=np.float32)
 
@@ -97,15 +102,14 @@ class UserItemBPRHybridScoreRecommender(BaseRecommender):
                 scoresUser=self.userRecommender._compute_item_score(int(user_id), items_to_compute)
                 scoresItem=self.itemKNNCF._compute_item_score(int(user_id), items_to_compute)
                 scoresBPR=self.recommenderBPR._compute_item_score(int(user_id), items_to_compute)
-                
+                scoresP3=self.p3alpha._compute_item_score(int(user_id), items_to_compute)
 
-                item_weights[i]=np.array(sumScoresWeights(scoresUser,scoresItem,scoresBPR,self.ws1,self.ws2,self.ws3))    
+                item_weights[i]=np.array(sumScoresWeights(scoresP3,scoresUser,scoresBPR,scoresItem,
+                                        self.ws1,self.ws2,self.ws3,self.ws4))    
             
             #print(item_weights[i])     
             i += 1
         return item_weights
-
-    #serve a salvare il modello alla fine della ricderva di parametri bayesiana
     def save_model(self, folder_path, file_name=None):
         
         if file_name is None:
